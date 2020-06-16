e09d5868f0d16b56c2e0e42d91561a5c2df856c4
Vincent Lee <>
2020-06-15 19:47:35 -0700

    featured project component

--- src/components/featured-project.js
+++ src/components/featured-project.js
@@ -1,24 +1,18 @@
 import React from "react"
+import "./featured-project.scss"
 
 const FeaturedProject = () => (
-  <article className="flex flex-col w-full my-8 md:flex-row">
-    <img className="md:w-1/2 rounded-sm" />
-    <div className="w-full md:w-1/2">
-      <h4 className="text-brand-lightgreen m-0 text-right font-semibold">
-        Featured Project
-      </h4>
-      <h4 className="m-0 text-right font-semibold">Portfolio</h4>
+  <article className="featured-project">
+    <h3 className="featured-project__label">Featured Project</h3>
+    <h3 className="featured-project__title">Portfolio</h3>
+    <div className="featured-project__image" />
 
-      <div className="mt-12" style={{ direction: "rtl" }}>
-        <p
-          className="p-4 text-white bg-brand-darkgreen rounded-sm text-left"
-          style={{ width: "102%" }}
-        >
-          Description desc long featured project lorem ipsum carpe diem dolce
-          amore
-        </p>
-      </div>
-      <div className="text-right text-brand-darkgray mb-8">react nodejs</div>
+    <div className="featured-project__content">
+      <p className="featured-project__description">
+        Description desc long featured project lorem ipsum carpe diem dolce
+        amore
+      </p>
+      <div className="featured-project__skills">react nodejs</div>
     </div>
   </article>
 )
