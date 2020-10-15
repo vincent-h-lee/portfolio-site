b0bcb6ea88293af46e58dbe9a69032e491a69366
Vincent Lee <>
2020-10-14 21:21:14 -0700

    formatting

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,18 +8,16 @@ const NotFoundPage = () => (
     <SEO title="404: Not found" />
 
     <div className="hero bg-brand-darkgreen">
-        <div className="container">
-          <h1 className="text-white text-4xl md:text-5xl font-bold m-0">
+      <div className="container">
+        <h1 className="text-white text-4xl md:text-5xl font-bold m-0">
           Page not found
-          </h1>
-          <h2 className="text-white opacity-50 text-4xl md:text-5xl font-bold m-0">
-          </h2>
-          <p className="py-6 text-xl md:text-2xl text-gray-300 font-thin lg:w-1/2">
+        </h1>
+        <h2 className="text-white opacity-50 text-4xl md:text-5xl font-bold m-0"></h2>
+        <p className="py-6 text-xl md:text-2xl text-gray-300 font-thin lg:w-1/2">
           Sorry the page you're looking for doesn't exist
-
-          </p>
-        </div>
+        </p>
       </div>
+    </div>
   </Layout>
 )
 
