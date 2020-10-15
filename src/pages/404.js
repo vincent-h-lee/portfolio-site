8eadc914f721ca9d291caa03a2430bc715cbba2a
Vincent Lee <>
2020-10-14 21:20:49 -0700

    temp home page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,6 +6,20 @@ import SEO from "../layout/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
+
+    <div className="hero bg-brand-darkgreen">
+        <div className="container">
+          <h1 className="text-white text-4xl md:text-5xl font-bold m-0">
+          Page not found
+          </h1>
+          <h2 className="text-white opacity-50 text-4xl md:text-5xl font-bold m-0">
+          </h2>
+          <p className="py-6 text-xl md:text-2xl text-gray-300 font-thin lg:w-1/2">
+          Sorry the page you're looking for doesn't exist
+
+          </p>
+        </div>
+      </div>
   </Layout>
 )
 
