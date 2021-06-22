4b011bb0be9a093a6d222dfcc7442022efe2c58d
Vincent Lee <>
2021-06-22 10:33:46 -0700

    style: mobile hero screen height

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 export const Hero = ({ title, content }) => (
-  <section className="module pt-48">
+  <section className="module pt-48 min-h-screen md:min-h-0">
     <div className="container flex flex-col md:flex-row md:space-x-8">
       <div className="max-w-sm w-full">
         <h1 className="text-3xl font-bold lowercase">{title}</h1>
