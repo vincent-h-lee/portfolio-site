f0ac5eccbe6a3b3759ff825a500e638fc5efba70
Vincent Lee <>
2021-06-23 15:33:31 -0700

    style: hero mobile

--- src/modules/hero.js
+++ src/modules/hero.js
@@ -2,7 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 
 export const Hero = ({ title, content }) => (
-  <section className="module pt-64 min-h-screen md:min-h-0">
+  <section className="module pt-48 md:pt-64 min-h-screen md:min-h-0">
     <div className="container flex flex-col md:flex-row md:space-x-8">
       <div className="max-w-sm w-full">
         <h1 className="text-3xl font-bold lowercase">{title}</h1>
