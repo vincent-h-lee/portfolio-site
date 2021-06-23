f9d0f462d658ca2f24ecd863fbc9470f353f09fb
Vincent Lee <>
2021-06-23 15:16:56 -0700

    feat: cms styles

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,5 +1,6 @@
 import CMS from "netlify-cms-app"
 
 import IndexPagePreview from "./preview-templates/index-page-preview"
+import "../styles/index.scss"
 
 CMS.registerPreviewTemplate("index", IndexPagePreview)
