8fcea87caa4d9f33c72301dd67de2a453b4a1607
Vincent Lee <>
2022-05-16 18:05:59 -0700

    nav on mobile

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,41 +1,108 @@
 import {
   Box,
+  Button,
   Container,
+  Drawer,
+  DrawerBody,
+  DrawerCloseButton,
+  DrawerContent,
+  DrawerOverlay,
   Flex,
+  Hide,
   HStack,
   Link as ChakraLink,
+  Show,
+  useDisclosure,
+  VStack,
 } from "@chakra-ui/react";
 import Link from "next/link";
-import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { createRef, useRef } from "react";
+import { FaHamburger } from "react-icons/fa";
 
-export const Navigation = () => (
-  <Box borderBottom="1px" borderColor="blackAlpha.300" shadow="xs">
-    <Container
-      maxW="container.lg"
-      as="nav"
-      py="6"
-      fontSize="lg"
-      fontWeight="semibold"
-    >
-      <Flex justifyContent="space-between">
-        <Link href="/" passHref>
-          <ChakraLink>vincent</ChakraLink>
-        </Link>
-        <HStack spacing="4">
-          <ChakraLink href="https://github.com/vincent-h-lee" isExternal={true}>
-            github
-          </ChakraLink>
+const links = [
+  {
+    isExternal: true,
+    text: "github",
+    href: "https://github.com/vincent-h-lee",
+  },
+  {
+    isExternal: true,
+    text: "linkedin",
+    href: "https://linkedin.com/in/leevincenth",
+  },
+  {
+    href: "#contact",
+    text: "contact",
+  },
+];
 
-          <ChakraLink
-            href="https://linkedin.com/in/leevincenth"
-            isExternal={true}
-          >
-            linkedin
-          </ChakraLink>
+const btnRef = createRef();
 
-          <ChakraLink href="#contact">contact</ChakraLink>
-        </HStack>
-      </Flex>
-    </Container>
-  </Box>
-);
+export const Navigation = () => {
+  const btnRef = useRef(null);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box borderBottom="1px" borderColor="blackAlpha.300" shadow="xs">
+      <Container
+        maxW="container.lg"
+        as="nav"
+        py="6"
+        fontSize="lg"
+        fontWeight="semibold"
+      >
+        <Flex justifyContent="space-between">
+          <Link href="/" passHref>
+            <ChakraLink>vincent</ChakraLink>
+          </Link>
+
+          <Hide above="md">
+            <Button variant="ghost" ref={btnRef} onClick={onOpen}>
+              <FaHamburger aria-label="mobile menu toggle" size="24" />
+            </Button>
+
+            <Drawer
+              isOpen={isOpen}
+              placement="right"
+              onClose={onClose}
+              finalFocusRef={btnRef}
+            >
+              <DrawerOverlay />
+              <DrawerContent>
+                <DrawerCloseButton />
+
+                <DrawerBody>
+                  <VStack spacing="4" py="12" fontWeight="semibold">
+                    {links.map(({ text, href, isExternal }) => (
+                      <ChakraLink
+                        key={text}
+                        href={href}
+                        isExternal={isExternal ?? false}
+                      >
+                        {text}
+                      </ChakraLink>
+                    ))}
+                  </VStack>
+                </DrawerBody>
+              </DrawerContent>
+            </Drawer>
+          </Hide>
+
+          <Show above="md">
+            <HStack spacing="4">
+              {links.map(({ text, href, isExternal }) => (
+                <ChakraLink
+                  key={text}
+                  href={href}
+                  isExternal={isExternal ?? false}
+                >
+                  {text}
+                </ChakraLink>
+              ))}
+            </HStack>
+          </Show>
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
